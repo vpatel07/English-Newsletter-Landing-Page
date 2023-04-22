77f33fa4c8ec3525dfc6ba56812bcd8777802447
Vansh <vpatel077375@gmail.com>
2023-04-22 13:04:05 -0400

    reset

--- src/App.js
+++ src/App.js
@@ -1,37 +1,23 @@
-import React from 'react';
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const handleClick = (url) => {
-    window.open(url, '_blank');
-  };
-
   return (
-    <div className="App" style={{backgroundColor: '#212936'}}>
-      <div className="header">
-        <h1>AI Thing</h1>
-      </div>
-      <div className='text'>
-        <h4>
-          Weclome to our AI bot that allows you to do our Brain Tumor Decector.
-        </h4>
-      </div>
-      <div className="buttons" style={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '5vh'}}>
-        <input type="file" id="avatar" name="avatar" accept="image/png, image/jpeg">Choose File</input>
-        <p id="display-output">Output</p>
-      </div>
-      <div className='text'> 
-        This AI can detect: 
-        <ul>
-          <li>Scoliosis</li>
-          <li>Brain Tumors</li>
-          <li>Brain Hemorrages</li>
-        </ul> 
-      </div>
-        Why is this AI Useful:
-        <ul>
-          <li></li>
-        </ul>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
